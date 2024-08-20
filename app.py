085e8e5196140ed57f12f78fde12d60bff095dc3
Akindel Heroboy <shakiratakindel@gmail.com>
2024-08-20 08:11:01 +0100

    edited app.py

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+import os,requests
 from flask import Flask
+import telebot,time
+from telebot import types
+
+Token= os.environ.get('Token')
+bot= telebot.TeleBot(Token)
+
+
+
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/joke')
 def hello_world():
-    return 'Hello, World!'
+    url= "https://v2.jokeapi.dev/joke/Any?format=txt"
+    channel_id= ""
+    joke= requests.get(url).text
+    bot.send_message(channel_id,joke)
+    return joke
+
