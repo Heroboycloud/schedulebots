b0f0f4ee2b3d21f50d6f714d26e7c7c8304d5910
Akindel Heroboy <shakiratakindel@gmail.com>
2024-08-21 22:28:23 +0100

    re-edited app.py to include some stuff

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os,requests
 from flask import Flask
+from faker import Faker
 import telebot,time
 from telebot import types
 
 Token= os.environ.get('Token')
 bot= telebot.TeleBot(Token)
-
+fake= Faker()
 
 
 
@@ -15,7 +16,16 @@ app = Flask(__name__)
 def hello_world():
     url= "https://v2.jokeapi.dev/joke/Any?format=txt"
     channel_id= os.environ.get('channel_id')
-    joke= requests.get(url).text
+    headers= {"user-agent":fake.user_agent()}
+    joke= requests.get(url,headers=headers).text
     bot.send_message(channel_id,joke)
     return joke
 
+@app.route('/chuck_norris')
+def chuck():
+    url= "https://api.chucknorris.io/jokes/random"
+    channel_id= os.environ.get('channel_id')
+    headers= {"user-agent":fake.user_agent()}
+    joke= requests.get(url,headers=headers).json()
+    bot.send_message(channel_id,joke["value"])
+    return joke
