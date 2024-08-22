f0d31ee01ee3dd3670ef0dcc116629be871e1b73
Akindel Heroboy <shakiratakindel@gmail.com>
2024-08-22 09:59:08 +0100

    modified:   app.py 	new file:   util.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from faker import Faker
 import telebot,time
 from telebot import types
+import util
 
 Token= os.environ.get('Token')
 bot= telebot.TeleBot(Token)
@@ -14,18 +15,22 @@ app = Flask(__name__)
 
 @app.route('/joke')
 def hello_world():
+    title= "Daily Dose of Jokes"
     url= "https://v2.jokeapi.dev/joke/Any?format=txt"
     channel_id= os.environ.get('channel_id')
     headers= {"user-agent":fake.user_agent()}
     joke= requests.get(url,headers=headers).text
+    joke= util.message.format(title,joke)
     bot.send_message(channel_id,joke)
     return joke
 
 @app.route('/chuck_norris')
 def chuck():
+    title= "Chuck Norris Jokes"
     url= "https://api.chucknorris.io/jokes/random"
     channel_id= os.environ.get('channel_id')
     headers= {"user-agent":fake.user_agent()}
     joke= requests.get(url,headers=headers).json()
-    bot.send_message(channel_id,joke["value"])
+    joke= util.message.format(title,joke)
+    bot.send_message(channel_id,joke)
     return joke
---
+++ util.py
@@ -0,0 +1,15 @@
+
+
+message= '''
+*** Daily Dose of laughter🤣🤣🤣 ***
+==================================
+**Joke of the day**
+*{0}*
+
+*{1}*
+😁😁😁
+
+[Get more Jokes](http://schedulebots.render.com)
+
+
+'''
